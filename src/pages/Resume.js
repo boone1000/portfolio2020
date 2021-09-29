0861c275d3c680348b101cf45f9b0ef0af9f604d
Ashley Boone <ashley@Ashleys-MacBook-Pro.local>
2021-09-28 22:51:34 -0400

    update resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,12 +4,12 @@ function Resume(){
     return <div className="page">
         <div className='resumebody'>
 
-            <div id='resumebutton' onClick={ () => window.open(require('../images/AshleyBooneResume.pdf'), '_none')}>
+            <div id='resumebutton' onClick={ () => window.open(require('../images/AshleyBooneResume9.28.pdf'), '_none')}>
                 <p>Click here to download my resume</p>
             </div>
 
 
-            <iframe src={require('../images/AshleyBooneResume.pdf')} width="100%" height="1200px"></iframe>
+            <iframe src={require('../images/AshleyBooneResume9.28.pdf')} width="100%" height="1200px"></iframe>
 
         </div>
 
